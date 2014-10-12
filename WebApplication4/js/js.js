ca973656491f3d5d86e4a5eea4276769c0946a7c
TubalLara <tublargar@gmail.com>
2014-10-12 21:51:39 +0200

    cuadraditos

---
+++ WebApplication4/js/js.js
@@ -0,0 +1,35 @@
+﻿
+function leerInfo() {
+    if (localStorage && localStorage["nombre"]) {
+        nombre = eval(localStorage["nombre"]);
+        alert(nombre);
+    } else {
+        alert("Introduce Nombre")
+    }
+};
+
+
+function escribir() {
+       var nombre = document.querySelector("#txtNombre").value;
+
+       localStorage.setItem("nombre", JSON.stringify(nombre) );
+   
+
+
+};
+function borrar() {
+   
+    localStorage.removeItem("nombre");
+
+
+};
+
+(function () {
+  
+    document.querySelector("#btnGuardar").addEventListener("click", escribir);
+    document.querySelector("#btnBorrar").addEventListener("click", borrar);
+
+    leerInfo();
+   
+
+})();
